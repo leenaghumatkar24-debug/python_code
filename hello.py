1b324fff30b846c4b4b1347245a86031237c1834
Leena Ghumatkar <leena.ghumatkar24@gpcpolytenchnic.com>
2026-06-04 20:27:14 +0530

    my first python code  on git hub

---
+++ hello.py
@@ -0,0 +1,28 @@
+"""def Judge():
+    if 2>4:
+        print("2 is greater than 4")
+    else:
+        print("no 2 is not greater than 4")
+Judge()"""
+
+"""ef addition(a,b):
+    return a+b
+print(addition(2,6)"""
+
+"""List=[10,20,"sonal"]
+print(List)
+print(List[1])
+List.append("fellow")
+print(List)
+List.insert(1,"hi")
+print(List)"""
+
+t1=(1,2,6)
+t2=(2,3,7)
+res=t1+t2
+print(res)
+print(1 in t1)
+print(3 not in t2 )
+print(t1[1])
+print(t2[-1])
+
